99a167ec307fcf3b467ff07a86b8493263d63cdc
paulsvh <pvonhagen26@gmail.com>
2021-07-29 13:44:46 -0700

    Create person model and people controller

---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimCoreApi.Models
+{
+    public class Person
+    {
+
+        public int Id { get; set; } = 0;
+        public string FirstName { get; set; } = "";
+
+        public string LastName { get; set; } = "";
+    }
+}
